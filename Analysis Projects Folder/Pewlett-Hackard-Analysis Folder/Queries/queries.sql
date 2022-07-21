a769c5deaefb7043f2e307277d05cb716c6dc305
Aceofhearts1 <Drigger317@yahoo.com>
2022-07-21 17:13:35 -0400

    updated schema

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/queries.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/queries.sql
@@ -44,6 +44,20 @@ CREATE TABLE titles (
 	PRIMARY KEY (emp_no)
 );
 
+DROP TABLE titles;
+
+CREATE TABLE IF NOT EXISTS public.titles (
+    emp_no INTEGER NOT NULL,
+    title CHARACTER VARYING COLLATE pg_catalog."default" NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (emp_no, from_date, to_date),
+    CONSTRAINT fk_titles_emp_no FOREIGN KEY (emp_no)
+        REFERENCES public.employees (emp_no) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
 CREATE TABLE dept_employee(
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
@@ -60,7 +74,7 @@ DROP TABLE salaries CASCADE;
 DROP TABLE titles CASCADE;
 DROP TABLE dept_employee CASCADE;
 
-SELECT * FROM dept_employee;
+SELECT * FROM titles;
 
 SELECT first_name, last_name
 FROM employees
@@ -117,38 +131,80 @@ INNER JOIN dept_manager
 ON departments.dept_no = dept_manager.dept_no;
 
 -- Joining retirement_info and dept_emp tables
-SELECT retirement_info.emp_no,
-    retirement_info.first_name,
-	retirement_info.last_name,
-    dept_employee.to_date
-FROM retirement_info
-LEFT JOIN dept_employee
-ON retirement_info.emp_no = dept_employee.emp_no;
+-- with aliases
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_employee as de
+ON ri.emp_no = de.emp_no;
 
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_employee as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
 
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO dept_count
+FROM current_emp as ce
+LEFT JOIN dept_employee as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
 
+SELECT * FROM salaries;
 
+SELECT * FROM salaries
+ORDER BY to_date DESC;
 
+SELECT emp_no, first_name, last_name, gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
+DROP TABLE emp_info;
 
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary, de.to_date
+INTO emp_info
+FROM employees as e
+	INNER JOIN salaries as s
+		ON (e.emp_no = s.emp_no)
+	INNER JOIN dept_employee as de
+		ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	   AND (de.to_date = '9999-01-01');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+SELECT ce.emp_no, ce.first_name, ce.last_name, d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_employee AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
 
 
 
